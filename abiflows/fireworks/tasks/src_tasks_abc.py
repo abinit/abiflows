5379371a8a68ecf3226958a55cbcee8e6d2664d8
David Waroquiers <david.waroquiers@uclouvain.be>
2015-12-16 12:13:53 +0100

    Many changes ... towards SRC

--- abiflows/fireworks/tasks/src_tasks_abc.py
+++ abiflows/fireworks/tasks/src_tasks_abc.py
@@ -425,7 +425,7 @@ def createSRCFireworks(setup_task, run_task, control_task, spec=None, initializa
     control_spec = copy.deepcopy(spec)
     control_spec = set_short_single_core_to_spec(control_spec)
     control_spec['SRC_task_index'] = src_task_index
-    control_fw = Firework(control_task, spec=run_spec, name=src_task_index.control_str)
+    control_fw = Firework(control_task, spec=control_spec, name=src_task_index.control_str)
 
     links_dict = {setup_fw.fw_id: [run_fw.fw_id],
                   run_fw.fw_id: [control_fw.fw_id]}
