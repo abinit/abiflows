a11be1b6f3fd560e1c7db54e91b49425b5fc898d
David Waroquiers <david.waroquiers@uclouvain.be>
2015-12-17 11:59:01 +0100

    Removing the _launch_dir in ControlTask from the new_spec of the detours

--- abiflows/fireworks/tasks/src_tasks_abc.py
+++ abiflows/fireworks/tasks/src_tasks_abc.py
@@ -348,6 +348,8 @@ class ControlTask(SRCTaskMixin, FireTaskBase):
         new_spec['src_modified_objects'] = modified_objects
         new_spec.pop('_launch_dir')
         new_spec.pop('src_directories')
+        if '_queueadapter' in modified_objects:
+            new_spec['_queueadapter'] = modified_objects['_queueadapter']
         #TODO: what to do here ? Right now this should work, just transfer information from the run_fw to the
         # next SRC group
         if 'previous_fws' in fw_spec:
