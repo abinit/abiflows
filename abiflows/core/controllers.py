2dff74511460f7e3d030d7a87f1dcde44b168b2b
David Waroquiers <david.waroquiers@uclouvain.be>
2016-01-02 17:37:02 +0100

    UltimateMemoryController ...

--- abiflows/core/controllers.py
+++ abiflows/core/controllers.py
@@ -682,6 +682,116 @@ class MemoryController(Controller, QueueControllerMixin):
     #     return note
 
 
+class UltimateMemoryController(Controller, QueueControllerMixin):
+    """
+    Controller for memory infringements of the resource manager. The handler should be able to handle the possible
+    overhead of the master process.
+    """
+
+    is_handler = True
+    _controlled_item_types = [ControlledItemType.task_failed()]
+
+#
+#     def __init__(self, job_rundir='.', qout_file='queue.qout', qerr_file='queue.qerr', queue_adapter=None,
+#                  max_mem_per_proc_mb=8000, mem_per_proc_increase_mb=1000,
+#                  max_master_mem_overhead_mb=8000, master_mem_overhead_increase_mb=1000):
+
+    def __init__(self, max_mem_per_proc_mb=8000, mem_per_proc_increase_mb=1000,
+                       max_master_mem_overhead_mb=8000, master_mem_overhead_increase_mb=1000,
+                       memory_policy='physical_memory'):
+        """
+        Initializes the handler with the directory where the job was run, the standard output and error files
+        of the queue manager and the queue adapter used.
+
+        Args:
+            max_mem_per_proc_mb: Maximum memory per process in megabytes.
+            mem_per_proc_increase_mb: Amount of memory to increase the memory per process in megabytes.
+            max_master_mem_overhead_mb: Maximum overhead memory for the master process in megabytes.
+            master_mem_overhead_increase_mb: Amount of memory to increase the overhead memory for the master process
+                                             in megabytes.
+            memory_policy: Policy for the memory (some weird clusters sometimes use the virtual memory to stop jobs
+                           that overcome some virtual memory limit)
+        """
+        super(UltimateMemoryController, self).__init__()
+        self.max_mem_per_proc_mb = max_mem_per_proc_mb
+        self.mem_per_proc_increase_mb = mem_per_proc_increase_mb
+        self.max_master_mem_overhead_mb = max_master_mem_overhead_mb
+        self.master_mem_overhead_increase_mb = master_mem_overhead_increase_mb
+        self.memory_policy = memory_policy
+        self.priority = PRIORITY_VERY_LOW
+
+    @property
+    def memory_policy(self):
+        return self._memory_policy
+
+    @memory_policy.setter
+    def memory_policy(self, memory_policy):
+        if memory_policy not in ['physical_memory', 'virtual_memory']:
+            raise ValueError('Memory policy is "{}" in UltimateMemoryController while it should be either '
+                             '"physical_memory" or "virtual_memory"'.format(memory_policy))
+        self._memory_policy = memory_policy
+
+    def as_dict(self):
+        return {'@class': self.__class__.__name__,
+                '@module': self.__class__.__module__,
+                'max_mem_per_proc_mb': self.max_mem_per_proc_mb,
+                'mem_per_proc_increase_mb': self.mem_per_proc_increase_mb,
+                'max_master_mem_overhead_mb': self.max_master_mem_overhead_mb,
+                'master_mem_overhead_increase_mb': self.master_mem_overhead_increase_mb,
+                'memory_policy': self.memory_policy
+                }
+
+    @classmethod
+    def from_dict(cls, d):
+        return cls(max_mem_per_proc_mb=d['max_mem_per_proc_mb'],
+                   mem_per_proc_increase_mb=d['mem_per_proc_increase_mb'],
+                   max_master_mem_overhead_mb=d['max_master_mem_overhead_mb'],
+                   master_mem_overhead_increase_mb=d['master_mem_overhead_increase_mb'],
+                   memory_policy=d['memory_policy'])
+
+    @property
+    def skip_remaining_handlers(self):
+        return True
+
+    @property
+    def skip_lower_priority_controllers(self):
+        return True
+
+    def process(self, **kwargs):
+        # Get the Queue Adapter and the queue errors
+        queue_adapter = kwargs.get('queue_adapter', None)
+        if queue_adapter is None:
+            raise ValueError('No queue adapter passed to the UltimateMemoryController')
+        queue_errors = self.get_queue_errors(memory_policy=self.memory_policy, **kwargs)
+
+        # Create the Controller Note and the actions
+        note = ControllerNote(controller=self)
+        actions = {}
+
+        # No errors found
+        if queue_errors is not None:
+            raise ValueError('Error should have been caught by another controller ...')
+
+        # No memory error found
+        note.add_problem('Task has been stopped and we assume it is because of memory infringement by a slave process')
+        old_mem_per_proc = queue_adapter.mem_per_proc
+        if old_mem_per_proc == self.max_mem_per_proc_mb:
+            note.state = ControllerNote.ERROR_UNRECOVERABLE
+            note.add_problem('Maximum mem_per_proc has been reached, cannot increase further')
+            return note
+        new_mem_per_proc = old_mem_per_proc + self.mem_per_proc_increase_mb
+        if new_mem_per_proc > self.max_mem_per_proc_mb:
+            new_mem_per_proc = self.max_mem_per_proc_mb
+        actions['queue_adapter'] = Action(callable=queue_adapter.__class__.set_mem_per_proc,
+                                          mem_mb=new_mem_per_proc)
+
+        # Set the actions to be performed, the state and the type of restart
+        note.state = ControllerNote.ERROR_RECOVERABLE
+        note.actions = actions
+        note.reset_restart()
+        return note
+
+
 class SimpleValidatorController(Controller):
     """
     Simple validator controller to be applied after all other controllers (PRIORITY_LOWEST).
