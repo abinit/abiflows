81af13e4c1f5638960139c9f15dda4659506ea5d
David Waroquiers <david.waroquiers@uclouvain.be>
2016-01-11 11:26:32 +0100

    Correction in get_final_structure_and_history of RelaxFWWorkflowSRC

--- abiflows/fireworks/workflows/abinit_workflows.py
+++ abiflows/fireworks/workflows/abinit_workflows.py
@@ -438,12 +438,15 @@ class RelaxFWWorkflowSRC(AbstractFWWorkflow):
         if final_fw_id is None:
             raise RuntimeError('Final structure not found ...')
         myfw = wf.id_fw[final_fw_id]
+        mytask = myfw.tasks[-1]
         #TODO add a check on the state of the launches
         last_launch = (myfw.archived_launches + myfw.launches)[-1]
         #TODO add a cycle to find the instance of AbiFireTask?
         # myfw.tasks[-1].set_workdir(workdir=last_launch.launch_dir)
-        helper = myfw.tasks[-1].task_helper
-        helper.set_task(myfw.tasks[-1])
+        mytask.setup_rundir(last_launch.launch_dir, create_dirs=False)
+        helper = mytask.task_helper
+        helper.set_task(mytask)
+
         structure = helper.get_final_structure()
         history = loadfn(os.path.join(last_launch.launch_dir, 'history.json'))
 
