aed1c3f7241a878fa0309203757a5cf6205191c6
David Waroquiers <david.waroquiers@uclouvain.be>
2016-02-04 14:54:17 +0100

    utils factors to get the lowest number NN higher than number MM that is factorizable by some given factors (i.e. 2, 3 and 5)

---
+++ abiflows/utils/factors.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+"""
+Utilities to get factors of a given number or related things ...
+"""
+
+import itertools
+import math
+
+def lowest_nn_gte_mm(mm, factors):
+    if mm < 1:
+        raise ValueError('Number mm should be >= 1')
+    if not any([factor > 1 for factor in factors]):
+        raise ValueError('At least one factor should be > 1')
+    if len(set(factors)) != len(factors):
+        raise ValueError('The factors should all be different')
+    lowest = -1
+    current_exponents = -1
+    max_exponents_factors = [int(math.ceil(math.log(float(mm), factor))) for factor in factors]
+
+    exponents_possibilities = [range(max_exponent+1) for max_exponent in max_exponents_factors]
+    for exponents in itertools.product(*exponents_possibilities):
+        nn = 1
+        for ifactor, factor in enumerate(factors):
+            nn *= factor ** exponents[ifactor]
+        if nn < mm:
+            continue
+        elif nn == mm:
+            lowest = nn
+            current_exponents = exponents
+            break
+        else:
+            if lowest == -1:
+                lowest = nn
+                current_exponents = exponents
+            elif nn < lowest:
+                lowest = nn
+                current_exponents = exponents
+
+    return lowest, current_exponents
---
+++ abiflows/utils/tests/test_factors.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+from __future__ import unicode_literals, division, print_function
+
+from abipy.core.testing import AbipyTest
+from abiflows.utils.factors import lowest_nn_gte_mm
+
+class TestFactors(AbipyTest):
+    def test_lowest_nn_gte_mm(self):
+        ll, exponents = lowest_nn_gte_mm(101, [2, 3, 5])
+        self.assertEqual(ll, 108)
+        self.assertEqual(exponents, (2, 3, 0))
+
+        ll, exponents = lowest_nn_gte_mm(100, [2, 3, 5])
+        self.assertEqual(ll, 100)
+        self.assertEqual(exponents, (2, 0, 2))
+
+        ll, exponents = lowest_nn_gte_mm(100, [2, 3, 5])
+        self.assertEqual(ll, 100)
+        self.assertEqual(exponents, (2, 0, 2))
+
+        ll, exponents = lowest_nn_gte_mm(101322142376, [7, 2, 13])
+        self.assertEqual(ll, 101322142376)
+        self.assertEqual(exponents, (8, 3, 3))
+
+        ll, exponents = lowest_nn_gte_mm(101322142375, [7, 2, 13])
+        self.assertEqual(ll, 101322142376)
+        self.assertEqual(exponents, (8, 3, 3))
+
+        ll, exponents = lowest_nn_gte_mm(101322142377, [7, 2, 13])
+        self.assertEqual(ll, 102820990636)
+        self.assertEqual(exponents, (11, 2, 1))
