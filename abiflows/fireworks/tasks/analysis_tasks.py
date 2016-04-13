7df8840022c32d4a504c36a29a1e83f907b4cfa5
David Waroquiers <david.waroquiers@uclouvain.be>
2016-04-13 11:46:23 +0200

    ChemenvLSETask

--- abiflows/fireworks/tasks/analysis_tasks.py
+++ abiflows/fireworks/tasks/analysis_tasks.py
@@ -3,6 +3,8 @@ from __future__ import print_function, division, unicode_literals
 import logging
 from fireworks.core.firework import FireTaskBase
 from pymatgen.analysis.chemenv.coordination_environments.coordination_geometry_finder import LocalGeometryFinder
+from pymatgen.analysis.chemenv.coordination_environments.structure_environments import LightStructureEnvironments
+from pymatgen.analysis.chemenv.coordination_environments.structure_environments import StructureEnvironments
 from pymatgen.matproj.rest import MPRester
 
 import json
@@ -66,35 +68,51 @@ class ChemEnvLightStructureEnvironmentsTask(FireTaskBase):
         logging.basicConfig(filename='chemenv_light_structure_environments.log',
                             format='%(levelname)s:%(module)s:%(funcName)s:%(message)s',
                             level=logging.DEBUG)
-        lgf = LocalGeometryFinder()
-        lgf.setup_parameters(centering_type='centroid', include_central_site_in_centroid=True,
-                             structure_refinement=lgf.STRUCTURE_REFINEMENT_NONE)
-        if 'chemenv_parameters' in fw_spec:
-            for param, value in fw_spec['chemenv_parameters'].items():
-                lgf.setup_parameter(param, value)
-        structure = fw_spec['structure']
+
         identifier = fw_spec['identifier']
-        lgf.setup_structure(structure)
-        se = lgf.compute_structure_environments_detailed_voronoi()
+        criteria = {'identifier': identifier}
+        # Where to get the full structure environments object
+        if fw_spec['structure_environments_setup'] == 'from_database':
+            se_database = fw_spec['structure_environments_database']
+            entry = se_database.collection.find_one(criteria)
+            gfs_fileobject = se_database.gridfs.get(entry['structure_environments'])
+            dd = json.load(gfs_fileobject)
+            se = StructureEnvironments.from_dict(dd)
+        elif fw_spec['structure_environments_setup'] == 'from_file':
+            se_filepath = fw_spec['structure_environments_filepath']
+            f = open(se_filepath, 'r')
+            dd = json.load(f)
+            f.close()
+            se = StructureEnvironments.from_dict(dd)
+        else:
+            raise RuntimeError('Wrong structure_environments_setup : '
+                               '"{}" is not allowed'.format(fw_spec['structure_environments_setup']))
+
+        # Compute the light structure environments object
+        chemenv_strategy = fw_spec['chemenv_strategy']
+        lse = LightStructureEnvironments(strategy=chemenv_strategy, structure_environments=se)
+
         if 'json_file' in fw_spec:
             json_file = fw_spec['json_file']
         else:
-            json_file = 'structure_environments.json'
+            json_file = 'light_structure_environments.json'
         f = open(json_file, 'w')
-        json.dump(se.as_dict(), f)
+        json.dump(lse.as_dict(), f)
         f.close()
         if 'mongo_database' in fw_spec:
             database = fw_spec['mongo_database']
             entry = {'identifier': identifier,
-                     'elements': [elmt.symbol for elmt in structure.composition.elements],
-                     'nelements': len(structure.composition.elements),
-                     'pretty_formula': structure.composition.reduced_formula,
-                     'nsites': len(structure)
+                     'elements': [elmt.symbol for elmt in lse.structure.composition.elements],
+                     'nelements': len(lse.structure.composition.elements),
+                     'pretty_formula': lse.structure.composition.reduced_formula,
+                     'nsites': len(lse.structure),
+                     'chemenv_statistics': lse.get_statistics(bson_compatible=True)
                      }
-            gridfs_msonables = {'structure': structure.as_dict(),
-                                'structure_environments': se.as_dict()}
-            if 'criteria' in fw_spec:
-                database.update_entry(query=fw_spec['criteria'], entry_update=entry,
+            gridfs_msonables = {'structure': lse.structure.as_dict(),
+                                'light_structure_environments': lse.as_dict()}
+            criteria = {'identifier': identifier}
+            if database.collection.find(criteria).count() == 1:
+                database.update_entry(query=criteria, entry_update=entry,
                                       gridfs_msonables=gridfs_msonables)
             else:
                 database.insert_entry(entry=entry, gridfs_msonables=gridfs_msonables)
