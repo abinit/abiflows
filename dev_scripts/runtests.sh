0b908f3600d87f0e4d80fdd428f9ca52364fc4bc
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2018-07-18 01:00:45 +0200

    Don't use workers for integration tests

--- dev_scripts/runtests.sh
+++ dev_scripts/runtests.sh
@@ -11,8 +11,8 @@ nosetests -v --with-coverage --cover-package=abiflows --logging-level=INFO --doc
 # This is to run the integration tests (append results)
 # integration_tests are excluded in setup.cfg
 if [[ "${ABIPY_COVERALLS}" == "yes" ]]; then 
-    pytest -n 2 --cov-config=.coveragerc --cov=abiflows --cov-append -v abiflows/fireworks/integration_tests 
-    #pytest --cov-config=.coveragerc --cov=abipy --cov-append -v abipy/integration_tests 
+    #pytest -n 2 --cov-config=.coveragerc --cov=abiflows --cov-append -v abiflows/fireworks/integration_tests 
+    pytest --cov-config=.coveragerc --cov=abiflows --cov-append -v abiflows/fireworks/integration_tests 
 fi
 
 # Generate documentation
