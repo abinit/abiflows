adc6898df4b824cfb083180fd85d1e4f410162d6
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2018-07-18 00:31:01 +0200

    Update documentation

--- CHANGELOG.rst
+++
@@ -1,2 +0,0 @@
-* :release:`0.2.0 <2017-03-10>`
-* :feature:`0` This is the first official release 
--- dev_scripts/flask_site/config.py
+++
@@ -1,3 +0,0 @@
-# coding: utf-8
-"""Configuration file."""
-from __future__ import print_function, division, unicode_literals
