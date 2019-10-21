771bdf0ba648da708dde7d83b749dbff6bfb474d
gmatteo <gmatteo@gmail.com>
2019-10-21 17:47:24 +0200

    Fix conda install

--- dev_scripts/runtests.sh
+++ dev_scripts/runtests.sh
@@ -5,13 +5,13 @@ set -e  # exit on first error, print each command
 #abinit --build
 abicheck.py --with-flow
 
-pytest --cov-config=.coveragerc --cov=abiflows -v --doctest-modules abiflows --ignore=abiflows/fireworks/integration_tests 
+pytest --cov-config=.coveragerc --cov=abiflows -v --doctest-modules abiflows --ignore=abiflows/fireworks/integration_tests
 
 # This is to run the integration tests (append results)
 # integration_tests are excluded in setup.cfg
-if [[ "${ABIPY_COVERALLS}" == "yes" ]]; then 
-    #pytest -n 2 --cov-config=.coveragerc --cov=abiflows --cov-append -v abiflows/fireworks/integration_tests 
-    pytest --cov-config=.coveragerc --cov=abiflows --cov-append -v abiflows/fireworks/integration_tests 
+if [[ "${ABIPY_COVERALLS}" == "yes" ]]; then
+    #pytest -n 2 --cov-config=.coveragerc --cov=abiflows --cov-append -v abiflows/fireworks/integration_tests
+    pytest --cov-config=.coveragerc --cov=abiflows --cov-append -v abiflows/fireworks/integration_tests
 fi
 
 # Generate documentation
